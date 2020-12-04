3a9dcd80c3a76d2ab2838e70edd258674a324cd7
orhansecic <orhan.secic@stu.ssst.edu.ba>
2020-12-04 15:05:57 +0100

    Finished Section 4. Part 49 (Ninth Commit - Both Changed)

--- projects/Section2-9/B3_GlobalEnvironment/Starter/app.js
+++ projects/Section2-9/B3_GlobalEnvironment/Starter/app.js
@@ -67,10 +67,6 @@ if(x){
 
 //Different Ways of Object Initialization
 
-
-
-
-
 /*
 var person = new Object();
 
@@ -107,7 +103,6 @@ var osoba = {
     zip : 10000,
   },
 };
-console.log(osoba);
 
 osoba.education = {
   elementarySchool : "Hasan Kikic",
@@ -116,6 +111,15 @@ osoba.education = {
 };
 osoba.DOB = null;
 
+console.log(osoba);
+
+var jsonOsoba = JSON.stringify(osoba);
+console.log(jsonOsoba);
+
+console.log(JSON.parse(jsonOsoba));
+*/
+
+/*
 var example = JSON.stringify(osoba);
 console.log(example);
 
@@ -132,6 +136,25 @@ objectFunction.newVariable = "Jos jedna Vrijednost";
 
 objectFunction();
 */
+
+
+//Functions are Objects
+/*
+function main(){
+  var english = "Hello";
+  var spanish = "Hola";
+  var bosnian = "Zdravo"
+
+  console.log(english + " " + main.ime);
+  console.log(spanish + " " + main.ime);
+  console.log(bosnian + " " + main.ime);
+
+
+  console.log(main.ime);
+};
+main.ime = "Orhan";
+main();
+*/
 /*
 "Function Statement"
 
@@ -141,7 +164,8 @@ function functionStatement() {
 }
 
 functionStatement();
-
+*/
+/*
 "Function Expression"
 
 var functionExpression = function(){
@@ -160,8 +184,25 @@ functionExpression.myFunction = function(){
   var b;
   console.log(b);
 }
+*/
+/*
+var foo = function (ime = "Timur"){
+  var english = "Hello, ";
+  var spanish = "Holla, ";
 
+  console.log(english + ime);
+  console.log(spanish + ime);
+  console.log(foo.bosnian + ime);
+  foo.foo2();
+};
+foo.bosnian = "Zdravo, ";
+foo.foo2 = function(){
+  console.log("Pa ovo radi!")
+};
+foo("Orhan");
+*/
 
+/*
 // Function parameters and Objects
 
 function myFunction(parameter1, parameter2){
@@ -253,6 +294,34 @@ myFunction();
 
 //THIS KEYWORD
 /*
+var a = "Orhan";
+
+example();
+function example(){
+  example2();
+    function example2(){
+      this.b = "Timur";
+      this.a = "Kerim";
+    }
+};
+
+var object = {
+  ime : "Mirela",
+  prezime : "Secic",
+
+  method : function(){
+    console.log(this);
+    var pointer = this;
+    stepBrother();
+    function stepBrother(){
+      console.log(pointer);
+    }
+  }
+}
+*/
+
+
+/*
 console.log(this);
 
 function anonymous() {
@@ -313,8 +382,8 @@ var object = {
     console.log(this);
     var objThis = this;
     funkcijaMetode();
-   
-    
+
+
 
       function funkcijaMetode(){
         console.log(this);
@@ -329,8 +398,8 @@ object.method();
 
 
 /* Arrays */
-/*
 
+/*
 var array = [
   1,
 
@@ -352,17 +421,18 @@ var array = [
 array[4](array[1]);
 
 array[4](array[3].name);
+console.log(array.length);
 */
 
 /* Arguments */
-/*
 
+/*
 function Greeting(firstname, lastname = "Kotromanic", age = "98", language = "BOS"){
-  
+
   if(arguments.length >= 4){
 
   console.log(firstname);
-  console.log(lastname);   
+  console.log(lastname);
   console.log(age);
   console.log(language);
   console.log("------------");
@@ -388,8 +458,8 @@ function returnExample(){
 
   var c = "Huba Buba";
 
-  return c; 
-  
+  return c;
+
 }
 
 console.log(returnExample());
@@ -402,6 +472,7 @@ var object = {
 
 /* Immediately Invoked Function Expressions */
 
+/*
 var firstName = "Orhan";
 
 (function (name) {
@@ -410,3 +481,130 @@ var firstName = "Orhan";
   console.log(greeting + name + "e");
 
 })(firstName);
+
+*/
+
+/* Typeof operator and parseInt */
+
+/*
+var a = window.prompt("Enter a value");
+var b = window.prompt("Enter a value");
+
+if(typeof a === "undefined" || "NaN" || "Null" && typeof b === "undefined"){
+  console.log("A or B do not have values");
+}else (example(parseInt(a, 10), parseInt(b, 10)));
+
+function example(a, b){
+  console.log(a + b);
+}
+*/
+
+/* for loop */
+/*
+console.log("It Works!");
+
+var even = " ";
+var odd = " ";
+for ( let x = 0; x<=10; x++){
+  if( x % 2 === 0){
+    even =even + x + ", ";
+  }else{
+    odd = odd + x + ", ";
+  }
+}
+console.log("Even numbers are: " + even);
+console.log("Odd numbers are: " + odd);
+*/
+
+/* Video 45. */
+/*
+var greeting = "Zdravo!";
+
+(function (name) {
+  var greeting = "Hello,";
+  greeting = "Đes ba,";
+  console.log(greeting + " " + name);
+
+}("Orhan"));
+
+console.log(greeting);
+*/
+
+/* Coersion */
+/*
+var string = "12";
+var number = 12;
+var boolean = true;
+*/
+//console.log(string + number); -> 1212
+//console.log(string + boolean); -> 12true
+//console.log(number + boolean); -> 13
+
+/* Closures */
+/*
+function main(greeting){
+
+  return function(firstname, lastname){
+    if(greeting === "en"){
+      return console.log("Hello, " + firstname + " " + lastname);
+    };
+    if(greeting === "es"){
+      return console.log("Hola, " + firstname + " " + lastname);
+    };
+  };
+};
+
+var foo = main("en");
+var foo1 = main("es");
+
+foo("Orhan", "Secic");
+foo1("Timur", "Secic");
+*/
+
+/* Callback Functions */
+/*
+function main(callback, callback2){
+  callback();
+  callback2();
+};
+
+var foo = function(){
+  console.log("I am a callback function.");
+};
+
+main(foo, function(){
+  console.log("I am also a freaking callback function.");
+});
+*/
+/* setTimeout function */
+/*
+var timer = setTimeout(function(){
+  console.log("Delayed message");
+}, 2000);
+console.log(timer);
+*/
+
+/* bind() method */
+//var ime = "Timur";
+//var prezime = "Doe";
+
+var object = {
+  ime : "Orhan",
+  prezime : "Secic",
+  fullname : function(){
+    return ("My name is " + this.ime + " " + this.prezime);
+  },
+};
+//Executing in the object and then saving the value outside of the object.
+var inside = object.fullname();
+console.log(inside);
+console.log("++++++++++++++++");
+// Executing outside without the bind method.
+var outside = object.fullname;
+console.log(outside());
+console.log("++++++++++++++++");
+// Executing outside of the object but using the bind method to connect to the object again
+var final = object.fullname;
+console.log(final());
+var foo = final.bind(object);
+console.log(foo());
